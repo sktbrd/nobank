53eb732c986f82a3c6512a7b2769cf9e59dfa9f6
¨sktbrd¨ <sktbrd.eth@gmail.com>
2024-02-23 15:05:02 -0700

    Map / Location / DummyLocation

--- App.js
+++ App.js
@@ -1,10 +1,55 @@
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      });
+    })();
+  }, []);
+
+  const dummyLocation = {
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Hey Highlander, check our mobile app!!!!!</Text>
+      <MapView
+        style={styles.map}
+        initialRegion={location}
+        showsUserLocation={true}
+        followsUserLocation={true}>
+        {location && (
+          <><Marker
+            coordinate={location}
+            title="I need cash here! You fuckers better come through!" />
+            <Marker
+              coordinate={dummyLocation}
+              title="BTC at $1.99 USD here" />
+          </>
+        )}
+      </MapView>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +58,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
